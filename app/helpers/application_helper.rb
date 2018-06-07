40b706ea2c0358c898668d2618c3af94bbca4818
Paulo Borba <pauloesb@gmail.com>
2018-06-07 17:41:14 -0300

    Implemented styling for blog nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,39 @@
 module ApplicationHelper
-  def login_helper style = ""
+  def login_helper style = "", ctag = false
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      login_ctag style, ctag
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      logout_ctag style, ctag
     end
+  end
+
+  def login_ctag style, ctag
+    if ctag
+      content_tag(:li, register_link(style), class: "nav-item") +
+        content_tag(:li, login_link(style), class: "nav-item")
+    else
+      register_link(style) + login_link(style)
+    end
+  end
+
+  def logout_ctag style, ctag
+    if ctag
+      content_tag(:li, login_link(style), class: "nav-item")
+    else
+      logout_link style
+    end
+  end
+
+  def register_link style
+    link_to "Register", new_user_registration_path, class: style
+  end
+
+  def login_link style
+    link_to "Login", new_user_session_path, class: style
+  end
+
+  def logout_link style
+    link_to "Logout", destroy_user_session_path, method: :delete, class: style
   end
 
   def source_helper
