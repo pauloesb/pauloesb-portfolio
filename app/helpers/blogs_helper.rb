e3551bfc7c89f9e0e5283fc9d88728794e1fa41f
Paulo Borba <pauloesb@gmail.com>
2018-06-08 18:54:42 -0300

    Added sidebar to show blog page and new post btn do sidebar

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -10,4 +10,8 @@ module BlogsHelper
   def show_published_time created_at
     content_tag(:span, blog_published_time(created_at), class: "meta") unless created_at.nil?
   end
+
+  def new_post_btn
+    link_to "New Post", new_blog_path, id: "blog-btn-new-post", class: "btn btn-light btn-block" if logged_in? :site_admin
+  end
 end
