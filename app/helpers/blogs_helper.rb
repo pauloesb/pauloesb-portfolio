0181b42ba8bbf13087d91ec5f5e1d2089e69bfdc
Paulo Borba <pauloesb@gmail.com>
2018-06-09 23:12:18 -0300

    Finished styling new and edit page and created btn helpers

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -12,6 +12,25 @@ module BlogsHelper
   end
 
   def new_post_btn
-    link_to "New Post", new_blog_path, id: "blog-btn-new-post", class: "btn btn-light btn-block" if logged_in? :site_admin
+    link_to "New Post",
+      new_blog_path,
+      id: "blog-btn-new-post",
+      class: "btn btn-primary btn-block" if logged_in? :site_admin
+  end
+
+  def img_asset_path img
+    asset_path img unless img.nil?
+  end
+
+  def back_btn
+    link_to "Back", blogs_path, class: "btn btn-link"
+  end
+
+  def edit_btn blog
+    link_to "Edit", edit_blog_path(blog), class: "btn btn-link" if logged_in? :site_admin
+  end
+
+  def show_btn blog
+    link_to "Show", blog, class: "btn btn-link"
   end
 end
