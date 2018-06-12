0812360f3c52edd1bcd1a4be35b4b983a731cc67
Paulo Borba <pauloesb@gmail.com>
2018-06-11 23:19:51 -0300

    Updated portfolio with new attr called position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -12,4 +12,6 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
+
+  scope :by_position, -> { order("position ASC") }
 end
