05132670cc47facd05e49ae41331ae03a5543dd5
Paulo Borba <pauloesb@gmail.com>
2018-06-11 23:11:36 -0300

    Refactored nav items into a nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,45 @@ module ApplicationHelper
   def set_copyright style = ""
     "<p class='#{style}'>&copy; #{Time.now.year} | <b>Paulo Borba</b> All rights reserved</p>".html_safe
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper tag_type: nil, tag_style: nil, link_style: nil
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type} class='#{tag_style}'>" unless tag_type.nil?
+      nav_links << "<a href='#{item[:url]}' class='#{link_style} #{active? item[:url]}'>#{item[:title]}</a>"
+      nav_links << "</#{tag_type}>" unless tag_type.nil?
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
