6d32b664d32ee1c41b943c5fddc51e356967f910
Paulo Borba <pauloesb@gmail.com>
2018-06-13 16:53:58 -0300

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -95,4 +95,16 @@ module ApplicationHelper
       image_generator(height: '600', width: '400')
     end
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg, title
+      js add_gritter(msg, title: title, sticky: false)
+  end
 end
